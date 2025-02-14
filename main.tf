433f81c671715f0e8912e0c758cd7af85d4f3d89
Mike Toscano <michael.andrew.toscano@gmail.com>
2025-02-13 23:30:24 -0500

    tf ci update

--- main.tf
+++ main.tf
@@ -20,12 +20,12 @@ provider "proxmox" {
   }
 }
 
-data "terraform_remote_state" "example" {
-  backend = "http"
+# data "terraform_remote_state" "example" {
+#   backend = "http"
 
-  config = {
-    address = var.example_remote_state_address
-    username = var.example_username
-    password = var.example_access_token
-  }
-}
+#   config = {
+#     address = var.example_remote_state_address
+#     username = var.example_username
+#     password = var.example_access_token
+#   }
+# }
