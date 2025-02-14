2fe9582c9465fa92ef8a01154aac6f75685db45d
Mike Toscano <michael.andrew.toscano@gmail.com>
2025-02-13 23:00:20 -0500

    fix ci file

--- main.tf
+++ main.tf
@@ -19,3 +19,13 @@ provider "proxmox" {
     username = "root"
   }
 }
+
+data "terraform_remote_state" "example" {
+  backend = "http"
+
+  config = {
+    address = var.example_remote_state_address
+    username = var.example_username
+    password = var.example_access_token
+  }
+}
--- vars.tf
+++ vars.tf
@@ -1,2 +1,17 @@
 variable "username" { type=string }
 variable "password" { type=string }
+
+variable "example_remote_state_address" {
+  type = string
+  description = "Gitlab remote state file address"
+}
+
+variable "example_username" {
+  type = string
+  description = "Gitlab username to query remote state"
+}
+
+variable "example_access_token" {
+  type = string
+  description = "GitLab access token to query remote state"
+}
