37e012afdfab1d8b53fe6ad91fbaca03b25e88df
Mike Toscano <michael.andrew.toscano@gmail.com>
2025-02-16 18:05:04 -0500

    update k8s install config and scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,14 +18,14 @@ sudo systemctl restart containerd
 sudo systemctl enable containerd
 
 # Add Kubernetes apt repository
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-# Update package index again after adding Kubernetes repo
-sudo apt update
-
-# Install Kubernetes packages (kubelet, kubeadm, kubectl)
-sudo apt install -y kubelet kubeadm kubectl
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo systemctl enable --now kubelet
 
 # Mark packages to hold at their current version
 sudo apt-mark hold kubelet kubeadm kubectl
