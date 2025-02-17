4c90404006a526d0bb0157900d52eac527815fad
Mike Toscano <michael.andrew.toscano@gmail.com>
2025-02-16 20:12:26 -0500

    update cloud image to regular oracular

--- vars.tf
+++ vars.tf
@@ -22,7 +22,7 @@ variable "datastore_id" {
 }
 variable "os_image" {
     type=string
-    default="https://cloud-images.ubuntu.com/minimal/releases/oracular/release/ubuntu-24.10-minimal-cloudimg-amd64.img"
+    default="https://cloud-images.ubuntu.com/oracular/current/oracular-server-cloudimg-amd64.img"
 }
 variable "pve_node" {
     type=string
