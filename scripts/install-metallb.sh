b7ccfb5ba164dc799dc83f51b9493c3bd7764933
Mike Toscano <michael.andrew.toscano@gmail.com>
2025-02-21 14:44:39 -0500

    redeploy -init

--- scripts/install-metallb.sh
+++ scripts/install-metallb.sh
@@ -6,7 +6,7 @@
 K8S1="${K8S1:?Error: K8S1 is not set (Master Node IP)}"
 METALLB_NAMESPACE="${METALLB_NAMESPACE:-metallb-system}"
 
-# --- Setup kubectl kubeconfig --- 
+# --- Setup kubectl kubeconfig ---
 mkdir -p ~/.kube
 scp -i key -o StrictHostKeyChecking=no k8s@"$K8S1":~/.kube/config ~/.kube/config
 export KUBECONFIG=~/.kube/config
