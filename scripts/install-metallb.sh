c085f675a85be63e0e0e26b9101962cd88bc2195
Mike Toscano <michael.andrew.toscano@gmail.com>
2025-02-21 13:37:07 -0500

    reset metallb install to helm --upgrade -init

--- scripts/install-metallb.sh
+++ scripts/install-metallb.sh
@@ -11,50 +11,8 @@ mkdir -p ~/.kube
 scp -i key -o StrictHostKeyChecking=no k8s@"$K8S1":~/.kube/config ~/.kube/config
 export KUBECONFIG=~/.kube/config
 
-# --- Helm Repository Setup ---
-echo "--- Helm Repository Setup ---"
-helm repo add metallb https://metallb.github.io/metallb
-helm repo update
-
-# # --- Create Metallb Namespace --- 
-# echo "--- Creating Metallb Namespace: ${METALLB_NAMESPACE} ---"
-# kubectl create namespace --dry-run=client -o yaml "$METALLB_NAMESPACE" | kubectl apply -f -
-
-# --- Install/Upgrade Metallb ---
-helm repo add metallb https://metallb.github.io/metallb
-helm upgrade --install metallb metallb/metallb -f metallb-config.yaml
-
-# --- Wait for webhook deployment to be ready ---
-echo "--- Waiting for metallb-webhook service to be ready ---"
-WEBHOOK_READY=false
-for i in {1..12}; do # Retry loop
-  kubectl get service -n "$METALLB_NAMESPACE" | grep metallb-webhook-service | wc -l
-  WEBHOOK_STATUS=$?
-  if [ "$WEBHOOK_STATUS" -eq 0 ]; then
-    WEBHOOK_READY=true
-    echo "metallb-webhook deployment is ready!"
-    break
-  else
-    echo "metallb-webhook deployment not yet ready, retry attempt $i..."
-    kubectl get pod -n "$METALLB_NAMESPACE" -l app=metallb,component=webhook # Check pod status during wait
-    sleep 10
-  fi
-done
-
-if [ "$WEBHOOK_READY" == false ]; then
-  echo "ERROR: metallb-webhook deployment failed to become ready after multiple retries. Exiting."
-  kubectl get deployment/metallb-webhook -n "$METALLB_NAMESPACE" -o yaml
-  kubectl get pods -n "$METALLB_NAMESPACE" -l app=metallb,component=webhook -o yaml
-  kubectl logs -n "$METALLB_NAMESPACE" -l app=metallb,component=webhook --all-containers
-  exit 1
-fi
-echo "metallb-webhook deployment readiness confirmed."
-
-sleep 5
-
-# # --- Apply IPAddressPool and L2Advertisement configurations ---
-# echo "--- Applying IPAddressPool and L2Advertisement configurations ---"
-# kubectl apply -f metallb-config.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/main/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/main/manifests/metallb.yaml
 
 sleep 5
 
