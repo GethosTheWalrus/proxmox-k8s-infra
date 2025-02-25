741452251022d317be385c4afe87bedcc66aaccf
Mike Toscano <michael.andrew.toscano@gmail.com>
2025-02-24 20:00:08 -0500

    redeploy -init

--- scripts/install-k8s.sh
+++ scripts/install-k8s.sh
@@ -61,6 +61,9 @@ echo "net.ipv4.ip_forward = 1" | sudo tee -a /etc/sysctl.conf
 echo "net.bridge.bridge-nf-call-iptables = 1" | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 
+modprobe br_netfilter
+sysctl -w net.bridge.bridge-nf-call-iptables = 1
+
 sleep 5
 
 echo "y" | sudo ufw enable 
