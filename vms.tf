d27a78e084e7ff45ed3fe46e31e97f10cc579d3b
Mike Toscano <michael.andrew.toscano@gmail.com>
2025-05-10 11:49:45 -0400

    redeploy -init

--- vms.tf
+++ vms.tf
@@ -123,6 +123,47 @@ resource "proxmox_virtual_environment_vm" "k8s3" {
   }
 }
 
+resource "proxmox_virtual_environment_vm" "k8s4" {
+  depends_on      = [proxmox_virtual_environment_vm.k8s1]
+  name            = "k8s4"
+  node_name       = var.pve_node
+  stop_on_destroy = true
+  initialization {
+    user_account {
+      keys        = [trimspace(tls_private_key.vm_key.public_key_openssh)]
+      username    = var.os_user
+      password    = var.os_password
+    }
+    ip_config {
+      ipv4 {
+        address = "192.168.69.83/24"
+        gateway = "192.168.69.1"
+      }
+    }
+  }
+
+  network_device {
+    model = "vmxnet3"
+  }
+
+  cpu {
+    cores = var.cpu_cores
+    type  = var.cpu_type
+  }
+
+  memory {
+    dedicated = var.dedicated_memory
+  }
+
+  disk {
+    datastore_id = var.datastore_id
+    file_id      = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = var.disk_size
+  }
+}
 
 resource "tls_private_key" "vm_key" {
   algorithm = "RSA"
