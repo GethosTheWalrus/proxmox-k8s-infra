52f42f1830bdc7277879f929685f902556061a0a
Mike Toscano <michael.andrew.toscano@gmail.com>
2025-07-30 15:16:09 -0400

    change cloud image to lts -init

--- vars.tf
+++ vars.tf
@@ -23,7 +23,7 @@ variable "datastore_id" {
 variable "os_image" {
     type=string
     default="https://cloud-images.ubuntu.com/daily/server/daily/server/daily/server/noble/current/noble-server-cloudimg-amd64.img"
-}
+} 
 variable "pve_node" {
     type=string
     default="proxmox1"
