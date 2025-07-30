f6466c737ad1303f756cc7093b287ddf990881f1
Mike Toscano <michael.andrew.toscano@gmail.com>
2025-07-30 15:55:47 -0400

    change cloud image to lts -init

--- vms.tf
+++ vms.tf
@@ -175,11 +175,10 @@ resource "tls_private_key" "vm_key" {
 }
 
 resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
-  content_type       = "vztmpl"
-  datastore_id       = var.os_image_datastore_id
-  node_name          = var.pve_node
-  url                = var.os_image
-  file_name          = "noble-server-cloudimg-amd64.img"
+  content_type   = "iso"
+  datastore_id   = var.os_image_datastore_id
+  node_name      = var.pve_node
+  url            = var.os_image
 }
 
 output "vm_private_key" {
