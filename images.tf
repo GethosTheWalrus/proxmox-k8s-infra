e69e5ab19e52c216292eff8980ff3bd9a032b710
Mike Toscano <michael.andrew.toscano@gmail.com>
2025-11-30 19:47:23 -0500

    Created images.tf: Automatically downloads the Ubuntu 24.04 LTS image. Updated vars.tf: Points to the new image. Updated .gitlab-ci.yml: Removed the hack that was deleting the image resource from state. -init

---
+++ images.tf
@@ -0,0 +1,7 @@
+resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
+  content_type = "iso"
+  datastore_id = "local"
+  node_name    = var.pve_node
+  url          = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
+  file_name    = "noble-server-cloudimg-amd64.img"
+}
